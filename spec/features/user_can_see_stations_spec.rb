74df6099573e22973d625af219ff98fad8c71fd2
earl-stephens <stephensearl@hotmail.com>
2019-05-13 09:23:22 -0600

    began layout of test; added search controller and search index view; adding gem pry; about to check params coming into search#index

---
+++ spec/features/user_can_see_stations_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+# You will be using this documentation:
+# https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/
+#
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80206 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see the total results of the stations that match my query, 90.
+# Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And the stations should only be public,
+# and not private, planned or temporarily unavailable.
+# And for each of the stations I should see
+# Name, Address, Fuel Types, Distance, and Access Times
+
+describe 'as a user visiting root' do
+  it 'shows total results' do
+    visit root_path
+
+    fill_in :q, with: 80206
+
+    click_on "Locate"
+
+    expect(page).to have_content("90 locations found")
+  end
+end
