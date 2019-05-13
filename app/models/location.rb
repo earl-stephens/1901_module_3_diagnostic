c9e95c8f66b48a14affa6a3dde35db52faeeb42d
earl-stephens <stephensearl@hotmail.com>
2019-05-13 11:07:57 -0600

    added fields to the model that the spec calls out

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,11 @@
 class Location
 # Name, Address, Fuel Types, Distance, and Access Times
   def initialize(location_data)
-    @name = 
+    @name = location_data["station_name"]
+    @address = location_data["street_address"]
+    @fuel_types = location_data["fuel_type_code"]
+    @distance = location_data["distance"]
+    @access_times = location_data["access_days_time"]
     binding.pry
   end
 
